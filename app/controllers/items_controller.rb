d541594c29adb31017adcde81073653815363818
Marcel Galang <marcelgalang@gmail.com>
2016-11-27 22:59:01 +0000

    Add views

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,54 @@
+class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+
+  def edit
+  end
+
+
+  def create
+    @list = List.find(params[:list_id])
+    @item = @list.items.build(item_params)
+    if @item.save
+      redirect_to list_path(@list)
+    else
+      render "lists/show"
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+
+    redirect_to list_path(@item.list)
+  end
+
+
+  def destroy
+    @item.destroy
+    redirect_to items_url, notice: 'Item was successfully destroyed.'
+
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def item_params
+      params.require(:item).permit(:description, :status, :list_id)
+    end
+end
---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,56 @@
+class ListsController < ApplicationController
+  # before_action :set_list, only: [:show, :edit, :update, :destroy]
+
+  # GET /lists
+  # GET /lists.json
+  def index
+    @lists = List.all
+    @list = List.new
+  end
+
+  # GET /lists/1
+  # GET /lists/1.json
+  def show
+    @list = List.find(params[:id])
+    @item = Item.new
+  end
+
+  # GET /lists/new
+  def new
+    @list = List.new
+    # @list.items.build
+  end
+
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_url(@list)
+    else
+      @lists = List.all
+      render :index
+    end
+  end
+
+
+  def destroy
+    @list.destroy
+    redirect_to lists_url, notice: 'List was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_list
+      @list = List.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def list_params
+      params.require(:list).permit(:name, :items_attributes => [:description])
+    end
+end
